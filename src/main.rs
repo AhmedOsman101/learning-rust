d2e34f75822bb4095571fe4d93658f1b656d7e8f
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-27 21:34:34 +0200

    feat: implement number guessing game
    - Generate random secret number
    - Read user input
    - Provide hints: "Bigger" or "Smaller"
    - Loop until the guess is correct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use rand;
+use rand::Rng;
 use std::io;
 
 fn main() {
   println!("---- Guessing Game ----\n");
 
-  let mut guess = String::new();
+  let secret_number: u32 = rand::rng().random_range(1..101);
+
+  let mut guess: String = String::new();
 
   println!("Enter a guess");
 
@@ -11,10 +15,31 @@ fn main() {
     .read_line(&mut guess)
     .expect("Failed to read line");
 
-  let guess: u32 = guess
+  let mut guess_value: u32 = guess
     .trim()
     .parse()
     .expect("Invalid input, expected a number");
 
-  println!("your guess is {}", guess);
+  while guess_value != secret_number {
+    if guess_value > secret_number {
+      println!("Smaller\n");
+    } else if guess_value < secret_number {
+      println!("Bigger\n");
+    }
+
+    println!("Enter a guess");
+
+    guess = String::new();
+
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
+
+    guess_value = guess
+      .trim()
+      .parse()
+      .expect("Invalid input, expected a number");
+  }
+
+  println!("\nYou guessed it right!");
 }
