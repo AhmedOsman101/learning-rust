9d6f8c8c0a304d944178930abb7fb67cc498976f
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-27 15:13:54 +0200

    (feat): read and parse the guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  println!("---- Guessing Game ----\n");
+
+  let mut guess = String::new();
+
+  println!("Enter a guess");
+
+  io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+  let guess: u32 = guess
+    .trim()
+    .parse()
+    .expect("Invalid input, expected a number");
+
+  println!("your guess is {}", guess);
 }
